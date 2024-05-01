e333283905d2fd25f2fcfbe449d4fa05bd85d09c
ShittyKopper <shittykopper@w.on-t.work>
2024-05-02 02:37:59 +0300

    Send default reactions as Like activities to Iceshrimp.NET instances

--- packages/backend/src/core/activitypub/ApRendererService.ts
+++ packages/backend/src/core/activitypub/ApRendererService.ts
@@ -283,9 +283,10 @@ export class ApRendererService {
 				if (instance && instance.softwareName === 'mastodon') isMastodon = true;
 				if (instance && instance.softwareName === 'akkoma')	isMastodon = true;
 				if (instance && instance.softwareName === 'pleroma') isMastodon = true;
+				if (instance && instance.softwareName === 'iceshrimp.net') isMastodon = true;
 			}
 		}
-		
+
 		const object: ILike = {
 			type: 'Like',
 			id: `${this.config.url}/likes/${noteReaction.id}`,
