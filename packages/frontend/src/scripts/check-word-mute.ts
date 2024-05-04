cf317da2449f831fd0c80b15df4013ce26e08e91
Hazel Koehler <acomputerdog@gmail.com>
2024-05-03 22:23:24 -0400

    revert refactor and cleanup

--- packages/frontend/src/scripts/check-word-mute.ts
+++ packages/frontend/src/scripts/check-word-mute.ts
@@ -5,41 +5,42 @@
 
 import type { Note, MeDetailed } from "misskey-js/entities.js";
 
-// TODO: this implementation is horribly inefficient.
-// Each regex is validated (using a regex, ironically), transformed, and then parsed - for each note being checked.
-// These regex objects should be cached somewhere.
-
 export function checkWordMute(note: Note, me: MeDetailed | null | undefined, mutedWords: Array<string | string[]>): boolean {
 	// 自分自身
 	if (me && (note.userId === me.id)) return false;
 
-	if (mutedWords.length < 1) return false;
+	if (mutedWords.length > 0) {
+		const text = getNoteText(note);
 
-	const text = getNoteText(note);
-	if (text === '') return false;
+		if (text === '') return false;
 
-	return mutedWords.some(filter => {
-		if (Array.isArray(filter)) {
-			// Clean up
-			const filteredFilter = filter.filter(keyword => keyword !== '');
-			if (filteredFilter.length === 0) return false;
+		const matched = mutedWords.some(filter => {
+			if (Array.isArray(filter)) {
+				// Clean up
+				const filteredFilter = filter.filter(keyword => keyword !== '');
+				if (filteredFilter.length === 0) return false;
 
-			return filteredFilter.every(keyword => text.includes(keyword));
-		} else {
-			// represents RegExp
-			const regexp = filter.match(/^\/(.+)\/(.*)$/);
+				return filteredFilter.every(keyword => text.includes(keyword));
+			} else {
+				// represents RegExp
+				const regexp = filter.match(/^\/(.+)\/(.*)$/);
 
-			// This should never happen due to input sanitisation.
-			if (!regexp) return false;
-
-			try {
-				return new RegExp(regexp[1], regexp[2]).test(text);
-			} catch (err) {
 				// This should never happen due to input sanitisation.
-				return false;
+				if (!regexp) return false;
+
+				try {
+					return new RegExp(regexp[1], regexp[2]).test(text);
+				} catch (err) {
+					// This should never happen due to input sanitisation.
+					return false;
+				}
 			}
-		}
-	});
+		});
+
+		if (matched) return true;
+	}
+
+	return false;
 }
 
 function getNoteText(note: Note): string {
