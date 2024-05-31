f412a074ff496fc9a9ea98d0ffe7e58f2d344991
dakkar <dakkar@thenautilus.net>
2024-05-31 11:25:58 +0100

    misskey-js update after our changes

--- packages/misskey-js/src/autogen/types.ts
+++ packages/misskey-js/src/autogen/types.ts
@@ -11426,6 +11426,13 @@ export type operations = {
           untilDate?: number;
           /** @default false */
           allowPartial?: boolean;
+          /** @default true */
+          withRenotes?: boolean;
+          /**
+           * @description Only show notes that have attached files.
+           * @default false
+           */
+          withFiles?: boolean;
         };
       };
     };
