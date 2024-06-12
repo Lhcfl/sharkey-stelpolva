d53df1c5d292f44cfe4d90830f12998ab726a582
KevinWh0 <45321184+ChaoticLeah@users.noreply.github.com>
2024-06-12 15:00:12 +0200

    undid weird / in search engine map

--- packages/frontend/src/scripts/search-engine-map.ts
+++ packages/frontend/src/scripts/search-engine-map.ts
@@ -1,12 +1,12 @@
 //store the URL and if its none of these its a custom one
 export const searchEngineMap = {
 	//The first one is the default search engine
-	'https://www.google.com/search/?q={query}': 'Google',
+	'https://www.google.com/search?q={query}': 'Google',
 	'https://duckduckgo.com/?q={query}': 'Duckduckgo',
-	'https://www.bing.com/search/?q={query}': 'Bing',
-	'https://search.yahoo.com/search/?p={query}': 'Yahoo',
-	'https://www.ecosia.org/search/?q={query}': 'Ecosia',
+	'https://www.bing.com/search?q={query}': 'Bing',
+	'https://search.yahoo.com/search?p={query}': 'Yahoo',
+	'https://www.ecosia.org/search?q={query}': 'Ecosia',
 	'https://www.qwant.com/?q={query}': 'Qwant',
-	'https://search.aol.com/aol/search/?q={query}': 'AOL',
-	'https://yandex.com/search/?text={query}': 'Yandex',
+	'https://search.aol.com/aol/search?q={query}': 'AOL',
+	'https://yandex.com/search?text={query}': 'Yandex',
 };
