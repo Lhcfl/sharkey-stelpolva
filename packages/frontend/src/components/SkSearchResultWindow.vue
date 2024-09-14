bd586c86ecec81836b860dbd71d13700665e099c
Esurio <esurio@esurio1673.net>
2024-09-14 16:41:31 +0900

    fix: use i18n in SkSearchResultWindow

--- packages/frontend/src/components/SkSearchResultWindow.vue
+++ packages/frontend/src/components/SkSearchResultWindow.vue
@@ -7,7 +7,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 <MkWindow ref="window" :initialWidth="600" :initialHeight="450" :canResize="true" @closed="emit('closed')">
 	<template #header>
 		<i class="ph-magnifying-glass ph-bold ph-lg" style="margin-right: 0.5em;"></i>
-		<b>Result</b>
+		<b>{{ i18n.ts.searchResult }}</b>
 	</template>
 	<MkNotes :key="props.noteKey" :pagination="props.notePagination"/>
 </MkWindow>
@@ -18,6 +18,7 @@ import { } from 'vue';
 import type { Paging } from '@/components/MkPagination.vue';
 import MkNotes from '@/components/MkNotes.vue';
 import MkWindow from '@/components/MkWindow.vue';
+import { i18n } from '@/i18n.js';
 
 const props = defineProps<{
 	noteKey: string | number | symbol | undefined;
