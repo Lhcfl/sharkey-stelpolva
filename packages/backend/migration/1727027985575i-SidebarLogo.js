5eedbe153ee12bf5f4960b422cd45edd1b34cdc8
piuvas <piuvas@proton.me>
2024-09-22 15:10:12 -0300

    sidebarlogo migration

---
+++ packages/backend/migration/1727027985575i-SidebarLogo.js
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: piuvas and other Sharkey contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+export class SidebarLogo1727027985575 {
+    name = 'SidebarLogo1727027985575';
+
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "meta" ADD "sidebarLogoUrl" character varying(1024)`);
+		await queryRunner.query(`ALTER TABLE "instance" ADD "sidebarLogoUrl" character varying(1024)`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "sidebarLogoUrl"`);
+		await queryRunner.query(`ALTER TABLE "instance" DROP COLUMN "sidebarLogoUrl"`);
+    }
+}
