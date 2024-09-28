d9ba0f9f0d7bd189651a7baa787f1b2e839c6594
丈槍由紀 <chikorita157@ateliershiori.moe>
2024-09-27 22:19:36 -0400

    Add back button to antenna, list, and tag timelines.

--- packages/frontend/src/pages/antenna-timeline.vue
+++ packages/frontend/src/pages/antenna-timeline.vue
@@ -5,7 +5,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 
 <template>
 <MkStickyContainer>
-	<template #header><MkPageHeader :actions="headerActions" :tabs="headerTabs"/></template>
+	<template #header><MkPageHeader :actions="headerActions" :displayBackButton="true" :tabs="headerTabs"/></template>
 	<MkSpacer :contentMax="800">
 		<div ref="rootEl">
 			<div v-if="queue > 0" :class="$style.new"><button class="_buttonPrimary" :class="$style.newButton" @click="top()">{{ i18n.ts.newNoteRecived }}</button></div>
