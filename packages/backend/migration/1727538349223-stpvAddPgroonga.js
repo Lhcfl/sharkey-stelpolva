9fd10d944f2bff4d42cdc2743aec59855bc29bec
Lhc_fl <linca@discourse.org>
2024-09-29 01:01:25 +0800

    fix: create if not exists for big table change

--- packages/backend/migration/1727538349223-stpvAddPgroonga.js
+++ packages/backend/migration/1727538349223-stpvAddPgroonga.js
@@ -3,16 +3,16 @@ export class StpvAddPgroonga1727538349223 {
 
 	async up(queryRunner) {
 		await queryRunner.query(
-			`CREATE INDEX "IDX_stpv_pgroonga_note_text" ON "note" USING "pgroonga" ("text")`,
+			`CREATE INDEX IF NOT EXISTS "IDX_stpv_pgroonga_note_text" ON "note" USING "pgroonga" ("text")`,
 		);
 		await queryRunner.query(
-			`CREATE INDEX "IDX_stpv_pgroonga_note_cw" ON "note" USING "pgroonga" ("cw" pgroonga_varchar_full_text_search_ops_v2)`,
+			`CREATE INDEX IF NOT EXISTS "IDX_stpv_pgroonga_note_cw" ON "note" USING "pgroonga" ("cw" pgroonga_varchar_full_text_search_ops_v2)`,
 		);
 		await queryRunner.query(
-			`CREATE INDEX "IDX_stpv_pgroonga_user_profile_desc" ON "user_profile" USING "pgroonga" ("description" pgroonga_varchar_full_text_search_ops_v2)`,
+			`CREATE INDEX IF NOT EXISTS "IDX_stpv_pgroonga_user_profile_desc" ON "user_profile" USING "pgroonga" ("description" pgroonga_varchar_full_text_search_ops_v2)`,
 		);
 		await queryRunner.query(
-			`CREATE INDEX "IDX_stpv_pgroonga_user_name" ON "user" USING "pgroonga" ("name" pgroonga_varchar_full_text_search_ops_v2)`,
+			`CREATE INDEX IF NOT EXISTS "IDX_stpv_pgroonga_user_name" ON "user" USING "pgroonga" ("name" pgroonga_varchar_full_text_search_ops_v2)`,
 		);
 	}
 
