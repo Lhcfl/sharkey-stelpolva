0a09e6d86a2feb03f59e00210fe36b509333f1d7
Hazel K <acomputerdog@gmail.com>
2024-09-29 22:50:39 -0400

    clean up copy constructor

--- packages/backend/src/models/LatestNote.ts
+++ packages/backend/src/models/LatestNote.ts
@@ -36,10 +36,8 @@ export class LatestNote {
 	public note: MiNote | null;
 
 	constructor(data: Partial<LatestNote>) {
-		if (data == null) return;
-
 		for (const [k, v] of Object.entries(data)) {
-			(this as any)[k] = v;
+			(this as Record<string, unknown>)[k] = v;
 		}
 	}
 }
