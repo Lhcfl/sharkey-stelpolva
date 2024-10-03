ef002e00b6088213c7bfbadda1d87a0a1212d2e9
dakkar <dakkar@thenautilus.net>
2024-10-03 18:51:05 +0100

    cover case of author speaking as cat while not being cat
    as @Marie pointed out, if you disable "is cat" without first disabling
    "speak as cat", you don't want to speak as a cat!

--- packages/frontend/src/components/global/MkMisskeyFlavoredMarkdown.ts
+++ packages/frontend/src/components/global/MkMisskeyFlavoredMarkdown.ts
@@ -58,7 +58,7 @@ export default function (props: MfmProps, { emit }: { emit: SetupContext<MfmEven
 	provide('linkNavigationBehavior', props.linkNavigationBehavior);
 
 	const isNote = props.isNote ?? true;
-	const shouldNyaize = props.nyaize === 'respect' && props.author?.speakAsCat && !defaultStore.state.disableCatSpeak;
+	const shouldNyaize = props.nyaize === 'respect' && props.author?.isCat && props.author?.speakAsCat && !defaultStore.state.disableCatSpeak;
 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 	if (props.text == null || props.text === '') return;
 
