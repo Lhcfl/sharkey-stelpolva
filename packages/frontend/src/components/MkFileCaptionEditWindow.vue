23b3edeb0482a4c73bbeecdcff687d023208f9f2
Lhc_fl <lhcfl@outlook.com>
2024-10-08 11:55:28 +0800

    ux: use firefish-style filecaptionwindow

--- packages/frontend/src/components/MkFileCaptionEditWindow.vue
+++ packages/frontend/src/components/MkFileCaptionEditWindow.vue
@@ -6,21 +6,41 @@ SPDX-License-Identifier: AGPL-3.0-only
 <template>
 <MkModalWindow
 	ref="dialog"
-	:width="400"
-	:height="450"
+	:width="9999"
+	:height="9999"
 	:withOkButton="true"
+	:withCloseButton="true"
 	:okButtonDisabled="false"
 	@ok="ok()"
 	@close="dialog?.close()"
 	@closed="emit('closed')"
 >
 	<template #header>{{ i18n.ts.describeFile }}</template>
-	<MkSpacer :marginMin="20" :marginMax="28">
-		<MkDriveFileThumbnail :file="file" fit="contain" style="height: 193px; margin-bottom: 16px;"/>
-		<MkTextarea v-model="caption" autofocus :placeholder="i18n.ts.inputNewDescription" @keydown="onKeydown($event)">
-			<template #label>{{ i18n.ts.caption }}</template>
-		</MkTextarea>
-	</MkSpacer>
+	<div class="container">
+		<div class="fullwidth top-caption">
+			<div class="mk-dialog">
+				<MkTextarea v-model="caption" autofocus autosize :placeholder="i18n.ts.inputNewDescription" @keydown="onKeydown($event)">
+					<template #label>{{ i18n.ts.caption }}</template>
+				</MkTextarea>
+			</div>
+		</div>
+		<div class="fullwidth img-container">
+			<header>{{ file.name }}</header>
+			<img
+				id="imgtocaption"
+				:src="file.url"
+				:alt="file.comment || undefined"
+				:title="file.comment || undefined"
+			/>
+			<footer>
+				<span>{{ file.type }}</span>
+				<span>{{ bytes(file.size) }}</span>
+				<span v-if="file.properties && file.properties.width">
+					{{ Number(file.properties.width) }}px × {{ Number(file.properties.height) }}px
+				</span>
+			</footer>
+		</div>
+	</div>
 </MkModalWindow>
 </template>
 
@@ -29,7 +49,7 @@ import { shallowRef, ref } from 'vue';
 import * as Misskey from 'misskey-js';
 import MkModalWindow from '@/components/MkModalWindow.vue';
 import MkTextarea from '@/components/MkTextarea.vue';
-import MkDriveFileThumbnail from '@/components/MkDriveFileThumbnail.vue';
+import bytes from '@/filters/bytes.js';
 import { i18n } from '@/i18n.js';
 
 const props = defineProps<{
@@ -60,3 +80,113 @@ async function ok() {
 	dialog.value?.close();
 }
 </script>
+
+<style lang="scss" scoped>
+.container {
+	display: flex;
+	inline-size: 100%;
+	block-size: calc(100% - 50px);
+	flex-direction: row;
+	overflow: scroll;
+	position: fixed;
+	inset-inline-start: 0;
+	inset-block-start: 50px;
+}
+// TODO: use logical property (max-inline-size doesn't work)
+@media (max-width: 850px) {
+	.container {
+		flex-direction: column;
+	}
+	.top-caption {
+		padding-block-end: 8px;
+	}
+}
+.fullwidth {
+	inline-size: 100%;
+	margin: auto;
+}
+.mk-dialog {
+	position: relative;
+	padding: 32px;
+	min-inline-size: 320px;
+	max-inline-size: 480px;
+	box-sizing: border-box;
+	text-align: center;
+	margin: auto;
+
+	> header {
+		margin-block-start: 0;
+		margin-inline-end: 0;
+		margin-block-end: 8px;
+		margin-inline-start: 0;
+		position: relative;
+
+		> .title {
+			font-weight: bold;
+			font-size: 20px;
+		}
+
+		> .text-count {
+			opacity: 0.7;
+			position: absolute;
+			inset-inline-end: 0;
+		}
+	}
+
+	> .buttons {
+		margin-block-start: 16px;
+
+		> * {
+			margin-block: 0;
+			margin-inline: 8px;
+		}
+	}
+
+	::v-deep(textarea) {
+		max-height: calc(-300px + 100vh);
+	}
+}
+.img-container {
+	display: flex;
+	flex-direction: column;
+	block-size: 100%;
+
+	> header,
+	> footer {
+		align-self: center;
+		display: inline-block;
+		padding-block: 6px;
+		padding-inline: 9px;
+		font-size: 90%;
+		background: rgba(0, 0, 0, 0.5);
+		border-radius: 6px;
+		color: #fff;
+	}
+
+	> header {
+		margin-block-end: 8px;
+		opacity: 0.9;
+	}
+
+	> img {
+		display: block;
+		flex: 1;
+		min-block-size: 0;
+		object-fit: contain;
+		inline-size: 100%;
+		cursor: zoom-out;
+		image-orientation: from-image;
+	}
+
+	> footer {
+		margin-block-start: 8px;
+		opacity: 0.8;
+
+		> span + span {
+			margin-inline-start: 0.5em;
+			padding-inline-start: 0.5em;
+			border-inline-start: solid 1px rgba(255, 255, 255, 0.5);
+		}
+	}
+}
+</style>
