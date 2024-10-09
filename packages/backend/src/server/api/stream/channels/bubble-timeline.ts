ebe5f678f6cf24d942255c73d681c2174ff2cba8
dakkar <dakkar@thenautilus.net>
2024-10-09 17:57:58 +0100

    fix imports in channels bubble-timeline

--- packages/backend/src/server/api/stream/channels/bubble-timeline.ts
+++ packages/backend/src/server/api/stream/channels/bubble-timeline.ts
@@ -7,6 +7,7 @@ import { Injectable } from '@nestjs/common';
 import { DI } from '@/di-symbols.js';
 import type { MiMeta } from '@/models/_.js';
 import type { Packed } from '@/misc/json-schema.js';
+import { MetaService } from '@/core/MetaService.js';
 import { NoteEntityService } from '@/core/entities/NoteEntityService.js';
 import { bindThis } from '@/decorators.js';
 import { RoleService } from '@/core/RoleService.js';
