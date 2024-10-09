5856858f4914d593a8aa4d23436452a65a0d6419
dakkar <dakkar@thenautilus.net>
2024-10-09 15:50:53 +0100

    try to fix icons

--- packages/frontend-embed/src/boot.ts
+++ packages/frontend-embed/src/boot.ts
@@ -6,8 +6,6 @@
 // https://vitejs.dev/config/build-options.html#build-modulepreload
 import 'vite/modulepreload-polyfill';
 
-import '@tabler/icons-webfont/dist/tabler-icons.scss';
-
 import '@/style.scss';
 import { createApp, defineAsyncComponent } from 'vue';
 import defaultLightTheme from '@@/themes/l-light.json5';
