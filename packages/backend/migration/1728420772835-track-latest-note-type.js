e3c79b0c83837f65185789a3ae17a0d94aee64a5
Hazelnoot <acomputerdog@gmail.com>
2024-10-11 09:41:49 -0400

    fix typos in track-latest-note-type migration

--- packages/backend/migration/1728420772835-track-latest-note-type.js
+++ packages/backend/migration/1728420772835-track-latest-note-type.js
@@ -8,9 +8,9 @@ export class TrackLatestNoteType1728420772835 {
 
     async up(queryRunner) {
 				await queryRunner.query(`ALTER TABLE "latest_note" DROP CONSTRAINT "PK_f619b62bfaafabe68f52fb50c9a"`);
-				await queryRunner.query(`ALTER TABLE "latest_note" ADD "isPublic" boolean NOT NULL DEFAULT false`);
-				await queryRunner.query(`ALTER TABLE "latest_note" ADD "isReply" boolean NOT NULL DEFAULT false`);
-				await queryRunner.query(`ALTER TABLE "latest_note" ADD "isQuote" boolean NOT NULL DEFAULT false`);
+				await queryRunner.query(`ALTER TABLE "latest_note" ADD "is_public" boolean NOT NULL DEFAULT false`);
+				await queryRunner.query(`ALTER TABLE "latest_note" ADD "is_reply" boolean NOT NULL DEFAULT false`);
+				await queryRunner.query(`ALTER TABLE "latest_note" ADD "is_quote" boolean NOT NULL DEFAULT false`);
 				await queryRunner.query(`ALTER TABLE "latest_note" ADD CONSTRAINT "PK_a44ac8ca9cb916faeefc0912abd" PRIMARY KEY ("user_id", is_public, is_reply, is_quote)`);
 		}
 
