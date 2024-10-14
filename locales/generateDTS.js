d04e44b55202f8f9f029b6aa5dd1f0fbdd37ce2c
Julia Johannesen <julia@insertdomain.name>
2024-10-14 15:15:24 -0400

    Fix indentation on locales/generateDTS.js

--- locales/generateDTS.js
+++ locales/generateDTS.js
@@ -57,8 +57,8 @@ function createMembers(record) {
 }
 
 export default function generateDTS() {
-  const sharkeyLocale = yaml.load(fs.readFileSync(`${__dirname}/../sharkey-locales/en-US.yml`, 'utf-8'));
-  const misskeyLocale = yaml.load(fs.readFileSync(`${__dirname}/ja-JP.yml`, 'utf-8'));
+	const sharkeyLocale = yaml.load(fs.readFileSync(`${__dirname}/../sharkey-locales/en-US.yml`, 'utf-8'));
+	const misskeyLocale = yaml.load(fs.readFileSync(`${__dirname}/ja-JP.yml`, 'utf-8'));
 	const locale = merge(misskeyLocale, sharkeyLocale);
 
 	const members = createMembers(locale);
